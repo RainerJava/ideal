661867ed18873203c550064fc61d2cec01ba537c
Jorge Viegas <jorgeviegas@live.com>
2015-10-06 18:35:39 -0300

    GODHUB

---
+++ src/crm/Conexao.java
@@ -0,0 +1,29 @@
+package crm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexao {
+
+    public Conexao() {
+    } //Possibilita instancias
+    public static Connection con = null;
+
+    public static void Conectar() {
+        System.out.println("Conectando ao banco...");
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://dbmy0063.whservidor.com:3306/agropecuar63", "agropecuar63", "9I5MCFIM3F");
+            System.out.println("Conectado.");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Classe não encontrada, adicione o driver nas bibliotecas.");
+            Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new RuntimeException(e);
+        }
+    }
+}
